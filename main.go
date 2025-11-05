853c760b78114c96924c2022c9a264cc31601d9b
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-11-05 14:38:36 +0100

    chore: bump version to 0.1.5

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/coollabsio/jean/tui"
 )
 
-const version = "0.1.4"
+const version = "0.1.5"
 
 // Global flag for debug logging (set after config is loaded)
 var debugLoggingEnabled bool = false
